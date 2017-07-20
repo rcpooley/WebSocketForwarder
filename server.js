cf941be04ed0ce817f9e83a2a0e0a86be61eeeb3
Robert Pooley <coolosity1998@gmail.com>
2017-07-20 13:03:24 +0200

    First commit with basic web server

---
+++ server.js
@@ -0,0 +1,28 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var config = require('./config.json');
+
+//Setup middleware
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//Handle get requests
+app.get('*', function (req, res) {
+    var url = req.url.split('?')[0];
+    if (url === '/')url += 'index.html';
+    var path = __dirname + '/public/' + url;
+    try {
+        fs.accessSync(path, fs.F_OK);
+    } catch (e) {
+        path = __dirname + '/public/err404.html';
+    }
+
+    res.sendFile(path);
+});
+
+//Start http server
+http.listen(config.httpport, function () {
+    console.log('Listening on *:' + config.httpport);
+});
